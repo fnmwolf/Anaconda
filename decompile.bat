4832bfe11ee1e7e7ccaae6e75266b6f032510ea3
fnmwolf <dankmemesreddit@outlook.com>
2021-07-30 22:15:35 -0400

    change python directory

--- decompile.bat
+++ decompile.bat
@@ -3,5 +3,5 @@ TITLE Decompiling...
 call cd %~dp0
 call python build.py build_ext --inplace
 cd "%~dp0tools" 
-"C:\Python2718\python.exe" bimbam.py %1 %2
+"C:\Python27\python.exe" bimbam.py %1 %2
 ECHO Done!
--- decompileAll.bat
+++ decompileAll.bat
@@ -5,7 +5,7 @@ call mkdir C:\Out
 call cd %~dp0Input
 for /R %%f in (*.exe) do (
 cd "%~dp0tools" 
-"C:\Python2718\python.exe" bimbam2.py "%%f"
+"C:\Python27\python.exe" bimbam2.py "%%f"
 )
 ECHO Done!
 PAUSE
